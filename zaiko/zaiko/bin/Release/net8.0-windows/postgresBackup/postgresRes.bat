5e99b47e7f1b328d4af8df2aa5e45349cbac341d
arisaEN <hkyamaha234@gmail.com>
2025-01-14 17:16:40 +0900

    Updated files with recent changes3

--- zaiko/zaiko/bin/Release/net8.0-windows/postgresBackup/postgresRes.bat
+++ zaiko/zaiko/bin/Release/net8.0-windows/postgresBackup/postgresRes.bat
@@ -1,15 +1,19 @@
 @echo off
-:: 作成日: 2025年1月14日16:56
+:: 作成日: 2025年1月14日17:00
+:: 環境変数の設定
 set PGUSER=arisa
 set PGPASSWORD=arisa
 set PGHOST=localhost
 set PGPORT=5432
 set DBNAME=SqueegeeManagement
-set BACKUP_DIR=C:\backup
-set BACKUP_FILE=C:\backup\backup_20250113_19.sql
+
+:: バッチファイルのあるディレクトリをリストアファイルおよびログファイルのディレクトリとして設定
+set SCRIPT_DIR=%~dp0
+set BACKUP_FILE=%SCRIPT_DIR%backup_20250113_19.sql
+set LOG_FILE=%SCRIPT_DIR%restore_error.log
 
 :: リストア実行
-"C:\Program Files\PostgreSQL\17\bin\pg_restore.exe" --clean -d %DBNAME% -U %PGUSER% -h %PGHOST% -p %PGPORT% -v "%BACKUP_FILE%" 2>> "%BACKUP_DIR%\restore_error.log"
+"C:\Program Files\PostgreSQL\17\bin\pg_restore.exe" --clean -d %DBNAME% -U %PGUSER% -h %PGHOST% -p %PGPORT% -v "%BACKUP_FILE%" 2>> "%LOG_FILE%"
 
 :: エラーチェック
 if %errorlevel% neq 0 (
